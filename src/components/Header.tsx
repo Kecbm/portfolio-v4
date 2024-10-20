fd670e6e88e7ca6a943215be77e3fd323ecb34ce
Klecianny Melo <kleciannymelo@gmail.com>
2024-10-19 22:27:55 -0300

    :sparkles: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
+import { HomeOutlined, UserOutlined, CodeOutlined, FileTextOutlined, ContactsOutlined } from '@ant-design/icons';
+import StyledHeader from './StyledHeader';
 const { Header: HeaderComponent } = Layout;
 
 const Header: React.FC = () => {
@@ -8,28 +10,35 @@ const Header: React.FC = () => {
   };
 
   return (
-    <HeaderComponent>
-        <Menu 
-            mode="horizontal"
-            onClick={handleClick}
-        >
-            <Menu.Item key="/">
-                Home
-            </Menu.Item>
-            <Menu.Item key="/about">
-                Sobre
-            </Menu.Item>
-            <Menu.Item key="/projects">
-                Projetos
-            </Menu.Item>
-            <Menu.Item key="/articles">
-                Artigos
-            </Menu.Item>
-            <Menu.Item key="/contact">
-                Contato
-            </Menu.Item>
-        </Menu>
-    </HeaderComponent>
+    <StyledHeader>
+        <HeaderComponent>
+            <Menu 
+                mode="horizontal"
+                onClick={handleClick}
+            >
+                <Menu.Item
+                    key="/"
+                    icon={<HomeOutlined />}
+                />
+                <Menu.Item
+                    key="/about"
+                    icon={ <UserOutlined /> }
+                />
+                <Menu.Item
+                    key="/projects"
+                    icon={ <CodeOutlined />}
+                />
+                <Menu.Item
+                    key="/articles"
+                    icon={ <FileTextOutlined />}
+                />
+                <Menu.Item
+                    key="/contact"
+                    icon={ <ContactsOutlined />}
+                />
+            </Menu>
+        </HeaderComponent>
+    </StyledHeader>
   );
 };
 
