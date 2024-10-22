6b223ce98611a55f279e4ee444a2e3c90649596d
Klecianny Melo <kleciannymelo@gmail.com>
2024-10-22 18:07:09 -0300

    :lipstick: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,42 +6,42 @@ const Footer: React.FC = () => {
 
   return (
     <StyledFooter>
-      <h1>Colaboração, Foco em Resultados e Aprendizado Contínuo.</h1>
-      Como desenvolvedora pleno, integro times de tecnologia com disposição para colaborar, compartilhar conhecimento e entregar produtos de alta qualidade que geram valor para o negócio.
+      <h1 id='title-ft-one'>Colaboração, Foco em Resultados e Aprendizado Contínuo.</h1>
+      <p id='sub-title-ft-one'>Como desenvolvedora pleno, integro times de tecnologia com disposição para colaborar, compartilhar conhecimento e entregar produtos de alta qualidade que geram valor para o negócio.</p>
 
       <br />
 
-      <DesktopOutlined />
-      Desenvolvedora Full Stack | 2+ anos de Experiência
-      <a href='#' target='_blank' rel='noreferrer'>Baixe meu CV e confira minhas qualificações</a>
+      <DesktopOutlined className='icon-ft' />
+      <p className='text-ft'>Desenvolvedora Full Stack | 2+ anos de Experiência</p>
+      <a href='#' target='_blank' rel='noreferrer' className='link-ft'>Baixe meu CV e confira minhas qualificações</a>
 
       <br />
 
-      <ReadFilled />
-      Autora de Artigos sobre Carreira, Soft Skills e Negócios
-      <a href='#' target='_blank' rel='noreferrer'>Leia meus insights no blog.</a>
+      <ReadFilled className='icon-ft' />
+      <p className='text-ft'>Autora de Artigos sobre Carreira, Soft Skills e Negócios</p>
+      <a href='#' target='_blank' rel='noreferrer' className='link-ft'>Leia meus insights.</a>
 
       <br />
 
-      <TeamOutlined />
-      Participante Ativa da Comunidade de Tecnologia
-      <a href='#' target='_blank' rel='noreferrer'>Confira meu perfil no Bluesky e fique por dentro das discussões mais recentes</a>
+      <TeamOutlined className='icon-ft' />
+      <p className='text-ft'>Participante Ativa da Comunidade de Tecnologia</p>
+      <a href='#' target='_blank' rel='noreferrer' className='link-ft'>Confira meu perfil no Bluesky e fique por dentro das discussões mais recentes</a>
 
       <br />
 
-      <h1>Compartilho Conhecimento e Construo o Futuro</h1>
+      <h2 id='title-ft-two'>Compartilho Conhecimento e Construo o Futuro.</h2>
 
       <br />
 
-      Conecte-se comigo e faça parte dessa jornada:
-      <LinkedinFilled />
-      <GithubFilled />
+      <p id='network-ft'>Conecte-se comigo e faça parte dessa jornada:</p>
+      <LinkedinFilled className='newtwork-icon-ft' />
+      <GithubFilled className='newtwork-icon-ft' />
       {/* Bluesky */}
-      <InstagramFilled />
+      <InstagramFilled className='newtwork-icon-ft' />
 
       <br />
 
-      Desenvolvido com <HeartFilled /> por <a href="https://github.com/kecbm">Klecianny Melo</a>
+      <p id='Copyright'>Desenvolvido com <HeartFilled /> por <a href="https://github.com/kecbm">Klecianny Melo</a></p>
     </StyledFooter>
   );
 };
--- src/components/StyledFooter.ts
+++ src/components/StyledFooter.ts
@@ -7,9 +7,47 @@ const StyledFooter = styled(AntFooter)`
   max-width: 100%;
   background-color: #000000;
   color: #FFFFFF;
-  height: 50vh;
+  max-height: fit-content;
   padding: 100px 400px 100px 400px;
   font-family: 'Roboto';
+
+  #title-ft-one {
+    font-size: 100px;
+  }
+
+  #sub-title-ft-one {
+    font-size: 20px;
+  }
+
+  .icon-ft {
+    font-size: 35px;
+  }
+
+  .text-ft {
+    font-size: 12px;
+  }
+
+  .link-ft {
+    font-size: 12px;
+    color: #F11052;
+  }
+
+  #title-ft-two {
+    font-size: 73px;
+  }
+
+  #network-ft {
+    font-size: 11px;
+  }
+
+  .newtwork-icon-ft {
+    font-size: 32px;
+  }
+
+  #Copyright {
+    font-size: 10px;
+    color: #777777;
+  }
 `;
 
 export { StyledFooter };
